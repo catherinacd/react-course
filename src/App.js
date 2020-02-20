dfbc20582157cec8860e2bb06c979db739022776
Ekaterina Dolgushina <Ekaterina.Dolgushina@noveogroup.com>
2020-02-20 21:44:24 +0700

    study react state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react'
 import Car from './Car/Car'
 class App extends Component {
   state = {
-    cars: [{ name: '', year: '' }]
+    cars: [
+      { name: 'Ford', year: '2018' },
+      { name: 'Audi', year: '1982' },
+      { name: 'Mazda', year: '1999' }
+    ],
+    pageTitle: 'React components'
   }
 
   render() {
+    const pageTitle = this.state.pageTitle
+    const cars = this.state.cars
+
     return (
       <div>
-        <h1>Hello Kate!</h1>
-        <Car name={'Audi'} year={2016}>
-          color
-        </Car>
+        <h1>{pageTitle}</h1>
+        {cars.map(({ name, year }, index) => (
+          <Car name={name} year={year} key={index}></Car>
+        ))}
       </div>
     )
   }
